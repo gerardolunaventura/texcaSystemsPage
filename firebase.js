8c1f5025738d8dbe8a50117438734b75e9c3ee80
gerardolunaventura <41306140+gerardolunaventura@users.noreply.github.com>
2026-03-14 23:51:45 -0600

    Update firebase.js
    setting credentias

--- firebase.js
+++ firebase.js
@@ -1,24 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
-
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
-
-import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
-
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-
-apiKey: "TU_API_KEY",
-authDomain: "TU_PROYECTO.firebaseapp.com",
-projectId: "TU_PROYECTO",
-storageBucket: "TU_PROYECTO.appspot.com",
-messagingSenderId: "XXXX",
-appId: "XXXX"
-
+  apiKey: "AIzaSyAEAnwzWAE1QFYOj2UxnjiOSKZNzmIedFk",
+  authDomain: "texcapage.firebaseapp.com",
+  projectId: "texcapage",
+  storageBucket: "texcapage.firebasestorage.app",
+  messagingSenderId: "626087641610",
+  appId: "1:626087641610:web:3526689bd253ed918fd5f3",
+  measurementId: "G-E6KXGSWGP5"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app); 
+const analytics = getAnalytics(app);
